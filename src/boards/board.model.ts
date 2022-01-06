cdc75bf900e3d7a99caaf658f539aeb470b4f2de
이호겸 <ihogyeom@ihogyeoms-MacBook-Air.local>
2022-01-06 13:47:22 +0900

    게시물 api작성완료 postman테스트 완료

---
+++ src/boards/board.model.ts
@@ -0,0 +1,14 @@
+//모델을 정의할때
+//interface :변수의 타입만을 체크합니다
+//class : 변수의타입체크와 인스턴스 새성도 가능하다
+
+export interface Board {
+  id: string;
+  title: string;
+  description: string;
+  status: BoardStatus;
+}
+export enum BoardStatus {
+  PUBLIC = 'PUBLIC',
+  PRIVATE = 'PRIVATE',
+}
